f20a3f6ec7ed83d81fae084ca317dcbf35e4af2e
NanoProgram <danielj.perezd@gmail.com>
2022-11-20 22:58:27 +0000

    Se incia ejercicio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function sujeto() {
+  let randomsujeto = Math.floor(Math.random() * 3);
+  return randomsujeto;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(sujeto());
 };
